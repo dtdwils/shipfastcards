7b255caf3e089352c4933a19aacc071a340afcd0
dtdwils <dave@wilsonmgi.com>
2025-11-21 16:17:21 -0800

    Update next.config.js - add
    Add next.config.js for TanStack compat

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,19 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    MEDUSA_BACKEND_URL: process.env.MEDUSA_BACKEND_URL,
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.externals = [
+        "@tanstack/react-query",
+        ...config.externals,
+      ];
+    }
+    const reactQueryPath = path.resolve(
+      require.resolve("@tanstack/react-query")
+    );
+    config.resolve.alias["@tanstack/react-query"] = reactQueryPath;
+    return config;
   },
 };
 
